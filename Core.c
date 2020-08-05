85580d85687e92401d17d397b96a3a9e54a02318
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-05 10:53:27 -0400

    Add files via upload

--- Core.c
+++ Core.c
@@ -1,5 +1,4 @@
 #include "Core.h"
-#include "Registers.h"
 
 Core *initCore(Instruction_Memory *i_mem)
 {
@@ -11,94 +10,144 @@ Core *initCore(Instruction_Memory *i_mem)
 
     // FIXME, initialize register file here.
     // core->data_mem[0] = ...
-	core -> data_mem[0] = 16;
-	core -> data_mem[1] = 128;
-	core -> data_mem[2] = 8;
-	core -> data_mem[3] = 4;
+    
+    //data memory setting for uint64_t arr[] = {16, 128, 8, 4}
+
+    //uint64_t arr[] = {16, 128, 8, 4};
+
+
+    core->data_mem[0] = 16;
+    core->data_mem[1] = 0;
+    core->data_mem[2] = 0;
+    core->data_mem[3] = 0;
+    core->data_mem[4] = 0;
+    core->data_mem[5] = 0;
+    core->data_mem[6] = 0;
+    core->data_mem[7] = 0;
+
+
+    core->data_mem[8] = 128;
+    core->data_mem[9] = 0;
+    core->data_mem[10] = 0;
+    core->data_mem[11] = 0;
+    core->data_mem[12] = 0;
+    core->data_mem[13] = 0;
+    core->data_mem[14] = 0;
+    core->data_mem[15] = 0;
+
+    core->data_mem[16] = 8;
+    core->data_mem[17] = 0;
+    core->data_mem[18] = 0;
+    core->data_mem[19] = 0;
+    core->data_mem[20] = 0;
+    core->data_mem[21] = 0;
+    core->data_mem[22] = 0; 
+    core->data_mem[23] = 0;
+
+    core->data_mem[24] = 4;
+    core->data_mem[25] = 0;
+    core->data_mem[26] = 0;
+    core->data_mem[27] = 0;
+    core->data_mem[28] = 0;
+    core->data_mem[29] = 0;
+    core->data_mem[30] = 0;
+    core->data_mem[31] = 0;
+
+
 
     // FIXME, initialize data memory here.
+    // core->reg_file[0] = ...
+
+    //set the reg_file
     core->reg_file[25] = 4;
-	core->reg_file[10] = 4;
-	core->reg_file[22] = 1;
+    core->reg_file[10] = 4;
+    core->reg_file[22] = 1;
 
     return core;
 }
 
 // FIXME, implement this function
 bool tickFunc(Core *core)
-{           
-
-	// *********** organized by processing stages (lecture notes #9 )****************
-	
-	//** instruction fetch **
-	// Steps may include
+{
+    // Steps may include
     // (Step 1) Reading instruction from instruction memory
     unsigned instruction = core->instr_mem->instructions[core->PC / 4].instruction;
-	Signal incremented_instruction = core->PC += 4;	
-	printf("inside tick function ");
-	//** decoding / reg reading  **
-	
-	// call control unit 
-	 Signal control_unit_input = (instruction / 64);
-	ControlSignals *signals = NULL;
-	ControlUnit(control_unit_input, signals);
-	// run immGen 
-		
-	Signal ImmeGen_sig = ImmeGen(instruction); // <------------------------------------ not finished, fix this!!!!!!!!!!!!!
+    
+	// (Step 2) ...
+	// prints instructions in decimal
+	printf("Instruction: %u\n", instruction);
+    
 
-	//get reg values
+    Signal input = (instruction & 127);
+	// prints opcode in decimal
 	
-	// get inputs for reg file from instructions
-	int reg_index_1,reg_index_2,write_register;
-	reg_index_1 = (instruction / 524288)>>15;
-	reg_index_2 = (instruction / 16777216)>>20;
-	write_register = (instruction / 2048)>>7;
-	Signal reg_read_1 = 0;
-	Signal reg_read_2 = 0;
-	if ( signals->RegWrite== 0 )
-	{
-		reg_read_1 = core->reg_file[reg_index_1];
-		reg_read_2 = core->reg_file[reg_index_2];
-	}
-	else if (signals->RegWrite == 1)		
-	{
-		core->reg_file[reg_index_1] = 0;// result of memory manipulation Mux all the way to the right
+    printf("Opcode: %ld\n", input); 
 	
-	}
+	//holds signals from the controller
+    ControlSignals signals;
+    ControlUnit(input, &signals);
 
-	//call 
-	// ** execute / address calc
-	// mux1
-	Signal mux_1_signal = MUX( signals->ALUSrc, reg_read_2,ImmeGen_sig);
-			
+    Signal func3 =( (instruction >> (7 + 5)) & 7);
+    
+	Signal func7 = ((instruction >> (7 + 5 + 3 + 5 + 5)) & 127);
+    Signal ALU_ctrl_signal = ALUControlUnit(signals.ALUOp, func7, func3);
+
+    Register reg_1 = (instruction >> (7 + 5 + 3)) & 31;
+    
+	Register reg_2 = (instruction >> (7 + 5 + 3 + 5)) & 31;
+
+    //create signal input to ALU from read data 1 output
+    Signal alu_in_0;
+    alu_in_0 = core->reg_file[reg_1];
+
+    Signal alu_in_1 = MUX(signals.ALUSrc,core->reg_file[reg_2],ImmeGen(instruction));
+    Signal ALU_output;
 	
+    Signal zero_alu_input;
+
+    ALU(alu_in_0, alu_in_1, ALU_ctrl_signal, &ALU_output, &zero_alu_input);
+    printf("ALU out: %ld\n", ALU_output);
+
+    Register write_reg = (instruction >> 7) & 31;
+
+    if(signals.MemWrite)
+    {
+        core->data_mem[ALU_output] = alu_in_1;
+    }
 	
-	//Alu control 
-	Signal aluControlResult = ALUControlUnit(signals->ALUOp, instruction>>24,instruction >> 11);
-	// alu 	
-	Signal *ALU_result = NULL;
-	Signal *zero = NULL;
-	ALU(reg_read_1,mux_1_signal,aluControlResult, ALU_result, zero);
+	// core outputs of memory 
+    Signal mem_result= 0;
+    
+    // (Step N) Increment PC. FIXME, is it correct to always increment PC by 4?!
+    // use mux to choose branch or incremented pc values   
+    mem_result|= core->data_mem[ALU_output + 7];
+    mem_result= mem_result<< 8 | core->data_mem[ALU_output + 6];
+    mem_result= mem_result<< 16 | core->data_mem[ALU_output + 5];
+    mem_result= mem_result<< 24 | core->data_mem[ALU_output + 4];
+    mem_result= mem_result<< 32 | core->data_mem[ALU_output + 3];
+    mem_result= mem_result<< 40 | core->data_mem[ALU_output + 2];
+    mem_result= mem_result<< 48 | core->data_mem[ALU_output + 1];
+    mem_result= mem_result<< 56 | core->data_mem[ALU_output + 0];
+    printf("%ld\n", mem_result);
+
+    if(signals.RegWrite)
+    {
+        core->reg_file[write_reg] = MUX(signals.MemtoReg, ALU_output, mem_result);
+    }
+
+
+
+    printf("Register x9 -  %ld\n", core->reg_file[9]); 
+    printf("Register x11 -  %ld\n", core->reg_file[11]);
+
+    Signal shifted_immediate = ShiftLeft1(ImmeGen(input));
 	
-	Signal branch_location = Add(incremented_instruction,aluControlResult<<1);
-	//** memory access 
+    core->PC = Add(core->PC, MUX((zero_alu_input & signals.Branch), 4, (signed int)shifted_immediate));
 	
-	Signal memory_result ;
-	memory_result = 0; // <----------------------- will fix when  dealing with ld 
-	Signal mux_2_signal = MUX(signals->MemtoReg, *ALU_result, memory_result); // fix this 
-	// <-------------------- figure out how i type loads results in  register 
-	
-	
-	
-	// mux 3
-	Signal mux_3_control = *zero && signals->Branch ;
-	Signal mux_3_signal = MUX( mux_3_control,incremented_instruction,branch_location);
-	//write results
-	core->reg_file[write_register] = mux_3_signal;
-	incremented_instruction = core->PC = mux_3_signal;
-	//printf("The data in register %x is %lx",write_register, core->reg_file[write_register]);
- 
- 
+    printf("ending Program Counter: %ld\n", core->PC);
+
+
+
     ++core->clk;
     // Are we reaching the final instruction?
     if (core->PC > core->instr_mem->last->addr)
@@ -113,8 +162,7 @@ void ControlUnit(Signal input,
                  ControlSignals *signals)
 {
     // For R-type
-    if (input == 51)
-    {
+    if (input == 51) {
         signals->ALUSrc = 0;
         signals->MemtoReg = 0;
         signals->RegWrite = 1;
@@ -123,40 +171,46 @@ void ControlUnit(Signal input,
         signals->Branch = 0;
         signals->ALUOp = 2;
     }
-	// For I-type -- ld
-    if (input == 3)
-    {
+    // For ld 
+    if (input == 3) { //opcode
         signals->ALUSrc = 1;
         signals->MemtoReg = 1;
         signals->RegWrite = 1;
         signals->MemRead = 1;
         signals->MemWrite = 0;
         signals->Branch = 0;
-        signals->ALUOp = 3;
+        signals->ALUOp = 0;
     }
-	
-	// For I-type -- slli <------------------ fix this
-    if (input == 19)
-    {
+    // For addi , slli 
+    if (input == 19){
         signals->ALUSrc = 1;
-        signals->MemtoReg = 0;
+        signals->MemtoReg = 1;
         signals->RegWrite = 1;
         signals->MemRead = 1;
         signals->MemWrite = 0;
         signals->Branch = 0;
-        signals->ALUOp = 3;
+        signals->ALUOp = 0;
     }
 	
-	// For sb-type -- bne <--------------------- fix this
-    if (input == 99)
-    {
+    // For sd (S-type)
+    if (input == 35){
+        signals->ALUSrc = 1;
+        signals->MemtoReg = 0; 
+        signals->RegWrite = 0;
+        signals->MemRead = 0;
+        signals->MemWrite = 1;
+        signals->Branch = 0;
+        signals->ALUOp = 0;
+    }
+    // For beq (SB-type)
+    if (input == 99){ //opcode
         signals->ALUSrc = 0;
-        signals->MemtoReg = 0;
+        signals->MemtoReg = 0; 
         signals->RegWrite = 0;
         signals->MemRead = 0;
         signals->MemWrite = 0;
         signals->Branch = 1;
-        signals->ALUOp = 0;
+        signals->ALUOp = 1;
     }
 }
 
@@ -165,25 +219,81 @@ Signal ALUControlUnit(Signal ALUOp,
                       Signal Funct7,
                       Signal Funct3)
 {
-    // For add
+    //  add
     if (ALUOp == 2 && Funct7 == 0 && Funct3 == 0)
     {
         return 2;
     }
-	// For ldd
-    if (ALUOp == 2 && Funct3 == 7)
+
+    
+
+    // For subtract 
+    if (ALUOp == 2 && Funct7 == 32 && Funct3 == 0)
+    {
+        return 6;
+    }
+    //and
+    if (ALUOp == 2 && Funct7 == 0 && Funct3 == 7)
+    {
+        return 0;
+    }
+    //  or 
+    if (ALUOp == 2 && Funct7 == 0 && Funct3 == 6)
+    {
+        return 1;
+    }
+
+    // ld 
+    if (ALUOp == 0)
+    {
+        return 2;
+    }
+    //  sd
+    if (ALUOp == 0)
+    {
+        return 2;
+    }
+    //  beq 
+    if (ALUOp == 1)
+    {
+        return 6;
+    }
+    // slli
+    if (ALUOp == 0 && Funct7 == 0 && Funct3 == 1)
     {
         return 3;
     }
-	
 }
 
 // FIXME (3). Imme. Generator
 Signal ImmeGen(Signal input)
 {
-	//long ImmeGen_var = (long )input;
-	return input;
-}
+    signed int immediate = 0;
+
+    //ld
+    if (input == 3){
+        // 000000000000;
+        immediate = 0;
+    }
+    //addi
+    if (input == 19){
+        //  000000000001;
+        immediate = 1;
+    }
+    //slli
+    if (input == 14)    {
+        //  000000000011;
+        immediate = 3;
+    }
+    //bne
+    if (input == 99)    {
+        //  111111111110;
+        immediate = -4;
+    }
+
+    return immediate;
+
+} 
 
 // FIXME (4). ALU
 void ALU(Signal input_0,
@@ -198,13 +308,31 @@ void ALU(Signal input_0,
         *ALU_result = (input_0 + input_1);
         if (*ALU_result == 0) { *zero = 1; } else { *zero = 0; }
     }
-	// For ld
-    if (ALU_ctrl_signal == 3)
+    // For and
+    if (ALU_ctrl_signal == 0)
     {
-        *ALU_result = input_1;
+        *ALU_result = (input_0 & input_1);
         if (*ALU_result == 0) { *zero = 1; } else { *zero = 0; }
     }
-	
+    // For or
+    if (ALU_ctrl_signal == 1)
+    {
+        *ALU_result = (input_0 | input_1);
+        if (*ALU_result == 0) { *zero = 1; } else { *zero = 0; }
+    }
+    // For subtraction
+    if (ALU_ctrl_signal == 6)
+    {
+        *ALU_result = (input_0 - input_1);
+        if (*ALU_result != 0) { *zero = 1; } else { *zero = 0; }
+    }
+    // For shift left
+    if (ALU_ctrl_signal == 3)
+    {
+        *ALU_result = (input_0 << input_1);
+        if (*ALU_result == 0) { *zero = 1; } else { *zero = 0; }
+    }
+
 }
 
 // (4). MUX
@@ -222,7 +350,7 @@ Signal Add(Signal input_0,
     return (input_0 + input_1);
 }
 
-// (6). ShiftLeft1
+// (6). ShiftLeft
 Signal ShiftLeft1(Signal input)
 {
     return input << 1;
